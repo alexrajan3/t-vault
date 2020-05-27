af73501c2031e92a2b72a2d4baf6b90897c2fb5d
alexanantha <63574989+alexanantha@users.noreply.github.com>
2020-05-27 13:24:32 +0530

    Remove maven docker build command as part of gitlab migration for CCP DR Job
    Remove maven docker build command as part of gitlab migration for CCP DR Job

--- build_tvault.sh
+++ build_tvault.sh
@@ -232,7 +232,7 @@ if [[ "$PKG_TYPE" == "docker" ]]; then
    echo "-----------------------------------------------------"
    cp $BASEDIR/tvault_all.tar.gz $DOCKER_FILE_DIR/
    cd $BASEDIR
-   mvn clean package docker:build
+   
 fi 
 
 echo "-----------------------------------------------------"
